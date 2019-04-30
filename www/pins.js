dc7d6df8d8c955d4288714a810ff9b7ea25c04c9
clorange <lorange.christian@gmail.com>
2019-04-30 10:53:54 -0600

    no message

--- www/pins.js
+++ www/pins.js
@@ -1,16 +1,16 @@
 var pin = [
-    {"name":"The Crown Pub","lat":"40.586255","lng":"-105.076543","url":"http://www.crownpub.net"},
-    {"name":"Social","lat":"40.5872801","lng":"-105.0783097","url":"http://www.socialfortcollins.com/"},
-    {"name":"Elliot's Martini Bar","lat":"40.5884398","lng":"-105.0766902","url":"http://www.elliotsmartini.com/"},
-    {"name":"Coopersmith's Pub & Brewing","lat":"40.587348","lng":"-105.07566","url":"http://www.coppermuse.com/"},
-    {"name":"The Mayor of Old Town","lat":"40.578889","lng":"-105.078713","url":"http://www.crownpub.net"},
-    {"name":"Trailhead Tavern","lat":"40.587235","lng":"-105.078429","url":"http://www.crownpub.net"},
-    {"name":"Bar Louie","lat":"40.542339","lng":"-105.072624","url":"http://www.crownpub.net"},
-    {"name":"CB & Potts","lat":"40.543631","lng":"-105.073941","url":"http://www.crownpub.net"},
-    {"name":"The Forge Public House","lat":"40.58881","lng":"-105.07551","url":"http://www.crownpub.net"},
-    {"name":"Tap & Handle","lat":"40.583739","lng":"-105.077551","url":"http://www.crownpub.net"},
-    {"name":"Blind Pig","lat":"40.588191","lng":"-105.074905","url":"http://www.crownpub.net"},
-    {"name":"Avogadro's Number","lat":"40.579534","lng":"-105.079213","url":"http://www.crownpub.net"}
+    {"name":"The Crown Pub","lat":"40.586255","lng":"-105.076543"},
+    {"name":"Social","lat":"40.5872801","lng":"-105.0783097"},
+    {"name":"Elliot's Martini Bar","lat":"40.5884398","lng":"-105.0766902"},
+    {"name":"Coopersmith's Pub & Brewing","lat":"40.587348","lng":"-105.07566"},
+    {"name":"The Mayor of Old Town","lat":"40.578889","lng":"-105.078713"},
+    {"name":"Trailhead Tavern","lat":"40.587235","lng":"-105.078429"},
+    {"name":"Bar Louie","lat":"40.542339","lng":"-105.072624"},
+    {"name":"CB & Potts","lat":"40.543631","lng":"-105.073941"},
+    {"name":"The Forge Public House","lat":"40.58881","lng":"-105.07551"},
+    {"name":"Tap & Handle","lat":"40.583739","lng":"-105.077551"},
+    {"name":"Blind Pig","lat":"40.588191","lng":"-105.074905"},
+    {"name":"Avogadro's Number","lat":"40.579534","lng":"-105.079213"}
 ];  
 
 
